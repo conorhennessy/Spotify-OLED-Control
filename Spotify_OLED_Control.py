9e76e11a08aba79dc53baf82598197fa3707f0d1
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2021-06-28 17:51:41 +0100

    Little tidy up refactor
    Update some variables names (constants and others)
    Fixed some spacing
    Fixed some spelling stuff

--- Spotify_OLED_Control.py
+++ Spotify_OLED_Control.py
@@ -33,11 +33,11 @@ Height = 64
 font_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "fonts", 'cour.ttf'))
 font = ImageFont.truetype(font_path, 18)
 
-scrollspeed = 4
-scrollbackspeed = 8
-songfontsize = 18
-artistfontsize = 15
-seekfontsize = 10
+SCROLL_SPEED = 4
+SCROLL_BACK_SPEED = 8
+SONG_FONT_SIZE = 18
+ARTIST_FONT_SIZE = 15
+SEEK_FONT_SIZE = 10
 
 # Get associated values from config file
 config = configparser.ConfigParser()
@@ -45,6 +45,7 @@ config.read('config.txt')
 credentials = config['credentials']
 
 global spotifyData
+
 
 class Spotify:
     def __init__(self):
@@ -97,13 +98,13 @@ class Spotify:
 
 
 class ScrollThread(threading.Thread):
-    def __init__(self, word, fontsize, ypos):
+    def __init__(self, word, fontsize, y_pos):
         threading.Thread.__init__(self)
         self.word = word
         self.end = False
         self.Width = Width
         self.x = 5
-        self.ypos = ypos
+        self.y_pos = y_pos
         self.font = ImageFont.truetype(font_path, fontsize)
         self.move = False  # True = right
         self.scrolling = False
@@ -118,12 +119,12 @@ class ScrollThread(threading.Thread):
 
     def run(self):  # scroll
         while True:
-            lastmove = self.move
+            last_move = self.move
             if self.scrolling and not self.end:  # This could be cleaner by only using one while loop & reverse variable
                 if self.move:
-                    self.x += scrollbackspeed
+                    self.x += SCROLL_BACK_SPEED
                 else:
-                    self.x -= scrollspeed  # TODO change this scroll logic so scrolls left continuously
+                    self.x -= SCROLL_SPEED  # TODO change this scroll logic so scrolls left continuously
 
                 if self.x < ((Width - self.w) - 10) and not self.move:  # Was moving left and has moved enough
                     self.move = True
@@ -132,26 +133,26 @@ class ScrollThread(threading.Thread):
                     if self.x > 0 and self.move:  # Was moving right and more than 0
                         self.move = False
 
-                if not self.move and lastmove:
+                if not self.move and last_move:
                     self.end = True
                     time.sleep(3)
             time.sleep(.2)
 
     def draw_obj(self):
-        draw.text((self.x, self.ypos), self.word, font=self.font, fill="white")
+        draw.text((self.x, self.y_pos), self.word, font=self.font, fill="white")
 
 
 class SeekThread(threading.Thread):
-    def __init__(self, current_pos, song_len, isplaying):
+    def __init__(self, current_pos, song_len, is_playing):
         threading.Thread.__init__(self)
         self.padding = 35
-        self.font = ImageFont.truetype(font_path, seekfontsize)
+        self.font = ImageFont.truetype(font_path, SEEK_FONT_SIZE)
         self.currentPos = current_pos
         self.lastTime = int(time.time())
         self.songLen = song_len
         self.end = False
-        self.isPlaying = isplaying
-        self.xpos = None
+        self.isPlaying = is_playing
+        self.x_pos = None
 
     def run(self):
         while True:
@@ -160,7 +161,7 @@ class SeekThread(threading.Thread):
             if self.isPlaying:
                 self.currentPos += diff
             percent = self.currentPos / self.songLen
-            self.xpos = self.padding + int(percent * (Width - self.padding * 2))
+            self.x_pos = self.padding + int(percent * (Width - self.padding * 2))
             if percent >= 1:
                 self.end = True
             else:
@@ -170,8 +171,8 @@ class SeekThread(threading.Thread):
     def draw_obj(self):
         m, s = divmod(int(self.songLen), 60)
         h, m = divmod(m, 60)
-        c_song_leng = '{:02d}:{:02d}'.format(m, s)
-        draw.text((Width - self.padding + 5, (Height - 11)), c_song_leng, font=self.font, fill="white")
+        c_song_len = '{:02d}:{:02d}'.format(m, s)
+        draw.text((Width - self.padding + 5, (Height - 11)), c_song_len, font=self.font, fill="white")
 
         m, s = divmod(int(self.currentPos), 60)
         h, m = divmod(m, 60)
@@ -182,20 +183,20 @@ class SeekThread(threading.Thread):
             # draw seek bar outline
             draw.rectangle((self.padding, (Height - 6), (Width - self.padding), (Height - 3)), "black", "white", 1)
             # draw bar within
-            draw.rectangle((self.padding, (Height - 5), (self.xpos + 2), (Height - 4)), "black", "white", 2)
+            draw.rectangle((self.padding, (Height - 5), (self.x_pos + 2), (Height - 4)), "black", "white", 2)
         else:
             # draw pause
             draw.rectangle((67, (Height - 11), 70, Height), "white", "white", 1)
             draw.rectangle((55, (Height - 11), 58, Height), "white", "white", 1)
 
 
-def remove_feat(trackname):
-    if "(feat." in trackname:
-        start = trackname.index("(feat")
-        end = trackname.index(")") + 2
-        return trackname.replace(trackname[start:end], "")
+def remove_feat(track_name):
+    if "(feat." in track_name:
+        start = track_name.index("(feat")
+        end = track_name.index(")") + 2
+        return track_name.replace(track_name[start:end], "")
     else:
-        return trackname
+        return track_name
 
 
 def concat_artists(artists):
@@ -209,17 +210,17 @@ def concat_artists(artists):
         return artists[0]["name"]
 
 
-def rotary_callback(channel):
+def rotary_callback():
     global clkLastState
-    clkState = GPIO.input(clk)
-    if clkState != clkLastState:
-        dtState = GPIO.input(dt)
-        if dtState != clkState and spotifyData.get_vol() < 100:
+    clk_state = GPIO.input(clk)
+    if clk_state != clkLastState:
+        dt_state = GPIO.input(dt)
+        if dt_state != clk_state and spotifyData.get_vol() < 100:
             spotifyData.sp.volume(spotifyData.get_vol() + 10)
         elif spotifyData.get_vol() > 0:
             spotifyData.sp.volume(spotifyData.get_vol() - 10)
-        print (spotifyData.get_vol())
-        clkLastState = clkState
+        print(spotifyData.get_vol())
+        clkLastState = clk_state
 
 
 if __name__ == "__main__":
@@ -234,13 +235,13 @@ if __name__ == "__main__":
         clkLastState = GPIO.input(clk)
         GPIO.add_event_detect(clk, GPIO.BOTH, callback=rotary_callback, bouncetime=100)
 
-        NETWORKTIMEOUT = 1
-        drawTime = datetime.now() + timedelta(seconds=NETWORKTIMEOUT)
+        NETWORK_TIMEOUT = 1
+        drawTime = datetime.now() + timedelta(seconds=NETWORK_TIMEOUT)
 
-        songScrollThread = ScrollThread(word=remove_feat(spotifyData.track), fontsize=songfontsize, ypos=5)
+        songScrollThread = ScrollThread(word=remove_feat(spotifyData.track), fontsize=SONG_FONT_SIZE, y_pos=5)
         songScrollThread.start()
 
-        artistScrollThread = ScrollThread(word=concat_artists(spotifyData.artists), fontsize=artistfontsize, ypos=30)
+        artistScrollThread = ScrollThread(word=concat_artists(spotifyData.artists), fontsize=ARTIST_FONT_SIZE, y_pos=30)
         artistScrollThread.start()
 
         with canvas(device) as draw:
@@ -291,7 +292,7 @@ if __name__ == "__main__":
                         spotifyData.get_playback()
                         seekThread.currentPos = spotifyData.progressMs / 1000
                         seekThread.isPlaying = spotifyData.isPlaying
-                        drawTime = datetime.now() + timedelta(seconds=NETWORKTIMEOUT)
+                        drawTime = datetime.now() + timedelta(seconds=NETWORK_TIMEOUT)
 
                         if spotifyData.track + spotifyData.artists[0]["name"] != lastSong:
                             break
@@ -309,7 +310,7 @@ if __name__ == "__main__":
                             else:
                                 songScrollThread.end = False
                                 artistScrollThread.end = False
-                                drawTime = datetime.now() + timedelta(seconds=NETWORKTIMEOUT)
+                                drawTime = datetime.now() + timedelta(seconds=NETWORK_TIMEOUT)
                 else:
                     pass
 
