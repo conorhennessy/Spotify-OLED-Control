a347dfca2304af30c2097f75123da5fe16fd05b5
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2021-07-01 21:17:29 +0100

    Continued refactor
    Addressed the following:
     - Renamed some variables
     - moar underscores in variable names
     - General formatting in code
     - Spelling fixes

--- Spotify_OLED_Control.py
+++ Spotify_OLED_Control.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 import os
-import threading
 import time
-from datetime import datetime
-from datetime import timedelta
+
+import multiprocessing as mp
+
 import configparser
+
 import spotipy
+
 from PIL import ImageFont
+
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import ssd1306
+
 from RPi import GPIO
-import multiprocessing as mp
-import time
-from threading import Thread
+
 
 clk = 17
 dt = 18
@@ -43,156 +45,156 @@ SONG_FONT_SIZE = 22
 ARTIST_FONT_SIZE = 18
 SEEK_FONT_SIZE = 10
 
-
 # Get associated values from config file
 config = configparser.ConfigParser()
 config.read('config.txt')
 credentials = config['credentials']
 
-global spotifyData
+global spotify_data
 
-class ScrollDataClass():
-    def __init__(self, textlen, string):
+
+class ScrollDataClass:
+    def __init__(self, text_len, string):
         self.x = 0
-        self.textlen = textlen
+        self.text_len = text_len
         self.string = string
-        self.scrolling  = textlen > Width
-        self.end = not self.scrolling #scroll has reached the end of its movement
-        self.movingLeft = True #scrolling direction
+        self.scrolling = text_len > Width
+        self.end = not self.scrolling  # scroll has reached the end of its movement
+        self.movingLeft = True  # scrolling direction
         self.restCounter = 0
 
+        # TODO modify scroll speed based on text length
 
-        #TODO modify scroll speed based on text length
-    def calcifEnded(self):
+    def calc_if_scroll_ended(self):
         if not self.end:
             if self.movingLeft:
-                if self.x < 0-self.textlen + Width:
+                if self.x < 0 - self.text_len + Width:
                     self.end = True
-                    #print("scroll ended")
             else:
                 if self.x >= 0:
                     self.end = True
-                    #print("scroll ended")
 
-class SeekbarDataClass():
+
+class SeekbarDataClass:
 
     def __init__(self, current_pos, song_len):
         self.currentPos = current_pos
         self.lastTime = int(time.time())
         self.songLen = song_len
-        self.totalTimeString = self.calcTimeString(song_len)
-        self.currentPosString = self.calcTimeString(self.currentPos)
+        self.totalTimeString = self.calc_time_string(song_len)
+        self.currentPosString = self.calc_time_string(self.currentPos)
         self.x_pos = 0
         self.padding = 35
         self.end = False
 
-    def calcTimeString(self, song_len):
+    def calc_time_string(self, song_len):
         m, s = divmod(song_len, 60)
         h, m = divmod(m, 60)
         c_song_len = '{:02d}:{:02d}'.format(m, s)
         return c_song_len
 
 
-class UIThread():
-    def __init__(self, spotifyData):
+class UIThread:
+    def __init__(self, spotify_data):
         self.dataAvailable = False
-        if spotifyData.track is not None and spotifyData.artists is not None:
-            self.track = spotifyData.track
-            self.artist = concat_artists(spotifyData.artists)
-            self.trackfont = ImageFont.truetype(font_path, SONG_FONT_SIZE)
-            self.artistfont = ImageFont.truetype(font_path, ARTIST_FONT_SIZE)
+        if spotify_data.track is not None and spotify_data.artists is not None:
+            self.track = spotify_data.track
+            self.artist = concat_artists(spotify_data.artists)
+            self.track_font = ImageFont.truetype(font_path, SONG_FONT_SIZE)
+            self.artist_font = ImageFont.truetype(font_path, ARTIST_FONT_SIZE)
             self.dataAvailable = True
 
-        self.seekBarFont = ImageFont.truetype(font_path, SEEK_FONT_SIZE)
-
+        self.seekbar_font = ImageFont.truetype(font_path, SEEK_FONT_SIZE)
 
     def run(self):  # scroll
         if self.dataAvailable:
             with canvas(device) as draw:
-                trackw, self.h = draw.textsize(self.track, font=self.trackfont)
-                artistw, self.h = draw.textsize(self.artist, font=self.artistfont)
+                track_w, self.h = draw.textsize(self.track, font=self.track_font)
+                artist_w, self.h = draw.textsize(self.artist, font=self.artist_font)
 
-            trackscrollinfo = ScrollDataClass(trackw, self.track)
-            artistscrollinfo = ScrollDataClass(artistw, self.artist)
-            scrollinfos = [trackscrollinfo, artistscrollinfo]
+            track_scroll_info = ScrollDataClass(track_w, self.track)
+            artist_scroll_info = ScrollDataClass(artist_w, self.artist)
+            scroll_infos = [track_scroll_info, artist_scroll_info]
 
-        seekbarinfo = SeekbarDataClass(int(spotifyData.progressMs/1000), int(spotifyData.durationMs/1000))
+        seekbar_info = SeekbarDataClass(int(spotify_data.progressMs / 1000), int(spotify_data.durationMs / 1000))
 
         while True:
             if self.dataAvailable:
-                scrollinfos = self.nextScrollFrame(scrollinfos)
+                scroll_infos = self.next_scroll_frame(scroll_infos)
 
-            seekbarinfo = self.nextSeekFrame(seekbarinfo, spotifyData.isPlaying)
+            seekbar_info = self.next_seek_frame(seekbar_info, spotify_data.isPlaying)
 
             with canvas(device) as draw:
                 if self.dataAvailable:
-                    #track name scroller
-                    draw.text((scrollinfos[0].x, 0), trackscrollinfo.string, font=self.trackfont, fill="white")
-                    #artist name scroller
-                    draw.text((scrollinfos[1].x, 24), artistscrollinfo.string, font=self.artistfont, fill="white")
+                    # track name scroller
+                    draw.text((scroll_infos[0].x, 0), track_scroll_info.string, font=self.track_font, fill="white")
+                    # artist name scroller
+                    draw.text((scroll_infos[1].x, 24), artist_scroll_info.string, font=self.artist_font, fill="white")
 
-                if not spotifyData.isPlaying:
+                if not spotify_data.isPlaying:
                     # draw pause symbol
                     draw.rectangle((67, (Height - 11), 70, Height), "white", "white", 1)
                     draw.rectangle((55, (Height - 11), 58, Height), "white", "white", 1)
                 else:
                     # draw seek bar outline
-                    draw.rectangle((seekbarinfo.padding, (Height - 6), (Width - seekbarinfo.padding), (Height - 3)), "black", "white", 1)
+                    draw.rectangle((seekbar_info.padding, (Height - 6), (Width - seekbar_info.padding), (Height - 3)),
+                                   "black", "white", 1)
                     # draw bar within
-                    draw.rectangle((seekbarinfo.padding, (Height - 5), (seekbarinfo.x_pos + 2), (Height - 4)), "black", "white", 2)
+                    draw.rectangle((seekbar_info.padding, (Height - 5), (seekbar_info.x_pos + 2), (Height - 4)),
+                                   "black", "white", 2)
 
-                #draw current time
-                draw.text((0, (Height - 11)), seekbarinfo.currentPosString, font=self.seekBarFont, fill="white")
-                #end time
-                draw.text((Width - seekbarinfo.padding + 5, (Height - 11)), seekbarinfo.totalTimeString, font=self.seekBarFont, fill="white")
+                # draw current time
+                draw.text((0, (Height - 11)), seekbar_info.currentPosString, font=self.seekbar_font, fill="white")
+                # end time
+                draw.text((Width - seekbar_info.padding + 5, (Height - 11)), seekbar_info.totalTimeString,
+                          font=self.seekbar_font, fill="white")
 
-
-
-    def nextScrollFrame(self, scrollinfos):
-        scrollingcount = 0
-        for scroll in scrollinfos:
-            scroll.calcifEnded()
+    def next_scroll_frame(self, scroll_infos):
+        scrolling_count = 0
+        for scroll in scroll_infos:
+            scroll.calc_if_scroll_ended()
             if scroll.scrolling:
-                scrollingcount += 1
+                scrolling_count += 1
 
         # true if both scroll bars are finished scrolling at the starting position
-        allended = scrollinfos[0].end and scrollinfos[0].movingLeft ==False and scrollinfos[1].end and scrollinfos[1].movingLeft == False
+        all_ended = scroll_infos[0].end and scroll_infos[0].movingLeft == \
+                    False and scroll_infos[1].end and scroll_infos[1].movingLeft == False
 
-        for scroll in scrollinfos:
+        for scroll in scroll_infos:
             if scroll.scrolling:
-                if scroll.end:  #if end of scrolling movement, on either side
-                        if scroll.movingLeft:
-                            scroll.end = False
-                            scroll.movingLeft = not scroll.movingLeft
-                        else:
-                            if allended or scrollingcount==1:       #if both scrolls back at begining or only one scrolling
-                                if scroll.restCounter > SCROLL_REST_TIME:
-                                    scroll.end=False
-                                    scroll.movingLeft = not scroll.movingLeft
-                                    scroll.restCounter = 0
-                                else:
-                                    scroll.restCounter+=1
+                if scroll.end:  # if end of scrolling movement, on either side
+                    if scroll.movingLeft:
+                        scroll.end = False
+                        scroll.movingLeft = not scroll.movingLeft
+                    else:
+                        if all_ended or scrolling_count == 1:  # if both scrolls back at beginning or only one scrolling
+                            if scroll.restCounter > SCROLL_REST_TIME:
+                                scroll.end = False
+                                scroll.movingLeft = not scroll.movingLeft
+                                scroll.restCounter = 0
+                            else:
+                                scroll.restCounter += 1
                 else:
                     if scroll.movingLeft:
                         scroll.x -= SCROLL_SPEED
                     else:
                         scroll.x += SCROLL_BACK_SPEED
-        return scrollinfos
+        return scroll_infos
 
-    def nextSeekFrame(self, seekbarinfo, isPlaying):
-        diff = time.time() - seekbarinfo.lastTime
-        seekbarinfo.lastTime = time.time()
-        if isPlaying:
-            seekbarinfo.currentPos += diff
-        percent = seekbarinfo.currentPos / seekbarinfo.songLen
-        seekbarinfo.x_pos = seekbarinfo.padding + int(percent * (Width - seekbarinfo.padding * 2))
+    def next_seek_frame(self, seekbar_info, is_playing):
+        diff = time.time() - seekbar_info.lastTime
+        seekbar_info.lastTime = time.time()
+        if is_playing:
+            seekbar_info.currentPos += diff
+        percent = seekbar_info.currentPos / seekbar_info.songLen
+        seekbar_info.x_pos = seekbar_info.padding + int(percent * (Width - seekbar_info.padding * 2))
         if percent >= 1:
-            seekbarinfo.end = True
+            seekbar_info.end = True
         else:
-            seekbarinfo.end = False
+            seekbar_info.end = False
 
-        seekbarinfo.currentPosString = seekbarinfo.calcTimeString(int(seekbarinfo.currentPos))
-        return seekbarinfo
+        seekbar_info.currentPosString = seekbar_info.calc_time_string(int(seekbar_info.currentPos))
+        return seekbar_info
 
     def finish(self):
         self.proc.terminate()
@@ -209,8 +211,8 @@ class Spotify:
         self.durationMs = None
         self.progressMs = None
         self.shuffleState = None
-        self.isPlaying = None               #play /pause
-        self.nothingPlaying = True          #a track is playing / no playback
+        self.isPlaying = None  # play /pause
+        self.nothingPlaying = True  # a track is playing / no playback
         self.volume = self.get_vol()
         if self.volume == 0:
             self.isMuted = True
@@ -241,26 +243,20 @@ class Spotify:
                     self.nothingPlaying = False
                 else:
                     pass
-                    # self.track = None
-                    # self.artists = None
-                    # self.durationMs = None
-                    # self.progressMs = None
 
                 # shuffle doesn't change
                 self.shuffleState = playback['shuffle_state']
             except TypeError:
                 self.nothingPlaying = True
-                print("type error getting data")
+                print("Type error getting data")
 
         except:
-            #TODO do something if this get hit lots
-            #print("AUGUGHGH CRASHED - something about timing out maybe?")
+            # TODO do something if this hits lots
             self.nothingPlaying = True
             self.track = None
             self.artists = None
             self.durationMs = None
             self.progressMs = None
-
 
     def get_vol(self):
         if self.isPlaying:
@@ -277,30 +273,29 @@ class Spotify:
             return "paused"
 
 
-def updateAllUis(ui, spotifyData):
-    print(spotifyData)
-    terminateThreads(ui)
-    ui = StartThreads(spotifyData)
-    return ui
+def update_all_UIs(UI, spotify_data):
+    print(spotify_data)
+    terminate_threads(UI)
+    UI = start_threads(spotify_data)
+    return UI
 
 
-def StartThreads(spotifyData):
-    # print("starting UI threads")
-    UIObjs = []
-    #TODO spotify data should not have None fields! where possible cache previous value
-    UIObjs.append(UIThread(spotifyData))
+def start_threads(spotify_data):
+    # print("Starting UI threads")
+    # TODO spotify data should not have None fields! where possible cache previous value
+    UI_objs = [UIThread(spotify_data)]
 
-
-    for i, obj in enumerate(UIObjs):
+    for i, obj in enumerate(UI_objs):
         p = mp.Process(target=obj.run)
         obj.proc = p
         p.start()
     mp.active_children()
-    return UIObjs
+
+    return UI_objs
 
 
-def terminateThreads(UIObjs):
-    for p in UIObjs:
+def terminate_threads(UI_objs):
+    for p in UI_objs:
         p.finish()
 
 
@@ -328,50 +323,50 @@ def concat_artists(artists):
 
 
 if __name__ == "__main__":
-    spotifyData = Spotify()
-    lastSong = ""
-    lastIsPlaying = False
-    lastSongPos = 0
+    spotify_data = Spotify()
+    last_song = ""
+    last_song_is_playing = False
+    last_song_pos = 0
     ui = []
-    spotifyData.get_playback()
-    print(spotifyData)
-    if spotifyData.isPlaying:
-        ui = StartThreads(spotifyData)
+    spotify_data.get_playback()
+    print(spotify_data)
+    if spotify_data.isPlaying:
+        ui = start_threads(spotify_data)
 
     while True:
         try:
-            lastSong = spotifyData.track + spotifyData.artists[0]["name"]
+            last_song = spotify_data.track + spotify_data.artists[0]["name"]
         except TypeError:
-            lastSong = None  # This means that nothing is playing
+            last_song = None  # This means that nothing is playing
 
-        if spotifyData.progressMs is not None:
-            lastSongPos = int(spotifyData.progressMs/1000)
-        lastIsPlaying = spotifyData.isPlaying
-        spotifyData.get_playback()
-        if spotifyData.nothingPlaying:
-            print("nothing playing")
+        if spotify_data.progressMs is not None:
+            last_song_pos = int(spotify_data.progressMs / 1000)
+        last_song_is_playing = spotify_data.isPlaying
+        spotify_data.get_playback()
+        if spotify_data.nothingPlaying:
+            print("Nothing playing")
             time.sleep(2)
             continue
 
-        #if spotifyData.track is None and lastSong is not None:  # paused
-        if spotifyData.isPlaying is False and lastIsPlaying is True:
-            #print("paused")
-            ui = updateAllUis(ui, spotifyData)
+        # if spotify_data.track is None and last_song is not None:  # paused
+        if spotify_data.isPlaying is False and last_song_is_playing is True:
+            print("Paused")
+            ui = update_all_UIs(ui, spotify_data)
             continue
 
-        #if lastSong is None and spotifyData.track is not None:  # unpaused
-        if spotifyData.isPlaying is True and lastIsPlaying is False:
-            print("unpaused")
-            ui = updateAllUis(ui, spotifyData)
+        # if last_song is None and spotify_data.track is not None:  # un-paused
+        if spotify_data.isPlaying is True and last_song_is_playing is False:
+            print("Un-paused")
+            ui = update_all_UIs(ui, spotify_data)
             continue
 
-        if spotifyData.isPlaying:
-            if spotifyData.track + spotifyData.artists[0]["name"] != lastSong:
+        if spotify_data.isPlaying:
+            if spotify_data.track + spotify_data.artists[0]["name"] != last_song:
                 print("Song changed")
-                ui = updateAllUis(ui, spotifyData)
+                ui = update_all_UIs(ui, spotify_data)
                 continue
 
-            if abs(lastSongPos - int(spotifyData.progressMs / 1000)) > 5:
-                print("woah time skipped")
-                ui = updateAllUis(ui, spotifyData)
+            if abs(last_song_pos - int(spotify_data.progressMs / 1000)) > 5:
+                print("woah! Time skipped")
+                ui = update_all_UIs(ui, spotify_data)
                 continue
